ef92f906f9f284b47c4cfd3c6fc01285ea466fb2
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-10-14 21:28:23 -0300

    update module: model fix list with dates

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/cdvelop/targetlist
 
 go 1.20
 
-require github.com/cdvelop/model v0.0.53
+require github.com/cdvelop/model v0.0.54
 
 replace github.com/cdvelop/model => ../model
--- html.go
+++ html.go
@@ -25,19 +25,20 @@ func (t TargetList) BuildTag() string {
 
 func (t TargetList) HtmlContainer() string {
 
-	return `<div class="container-list-search" data-id="` + t.Object.Name + `" onclick="TargetListSelected(event)">
-
-	<li data-id="1672322764831794600" class="target-li-off target-li"><a href="#" name="title">eco abdominal</a><span class="description-target">dra. sonia sandoval</span></li>
-
+	return `<div class="container-list-search">
+	<ol class="lista" data-id="` + t.Object.Name + `" onclick="TargetListSelected(event)">
+	</ol>
 	</div>
-
 	<div id="device-search-form" class="search-container">
-		<label for="filter-object">
-			<svg aria-hidden="true" focusable="false" class="form-btn">
-				<use xlink:href="#icon-search" />
-			</svg>
-		</label>
-		<input type="search" id="filter-object" title="letras números - permitidos max 50 caracteres"
-			pattern="^[A-Za-zÑñ 0-9-]{1,20}$">
+	<label for="filter-object">
+	<svg aria-hidden="true" focusable="false" class="form-btn">
+	<use xlink:href="#icon-search" />
+	</svg>
+	</label>
+	<input type="search" id="filter-object" title="letras números - permitidos max 50 caracteres"
+	pattern="^[A-Za-zÑñ 0-9-]{1,20}$">
 	</div>`
 }
+
+// <li data-id="1672322764831794600" class="target-li-off target-li"><a href="#" name="title">eco abdominal</a><span class="description-target">dra. sonia sandoval</span></li>
+// <li data-id="1672322764831794600" class="target-li-off target-li"><a href="#" name="title">eco abdominal</a><span class="description-target">dra. sonia sandoval</span></li>
